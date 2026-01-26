f0911078f7dcff04f4d56be5ef2a97bf3d6655f6
AmanGiri007 <giriaman4372gmail.com>
2026-01-26 20:33:05 +0545

    #day3

---
+++ day2.py
@@ -0,0 +1,31 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error,r2_score
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+
+np.random.seed(42)
+X=np.random.rand(100,1)*100
+y=3*X+np.random.randn(100,1)*2
+
+X_train,X_test,y_train,y_test= train_test_split(X,y,test_size=0.2,random_state=42)
+model=LinearRegression()
+model.fit(X_train,y_train)
+y_pred=model.predict(X_test)
+print("Slope:",model.coef_[0][0])
+print("Intercept:",model.intercept_[0])
+
+plt.scatter(X_test,y_test,color="blue",label="Actual")
+plt.plot(X_test,y_pred,color="red",label="Predicted")
+plt.title("Linear Regression Model")
+plt.xlabel("X")
+plt.ylabel("y")
+plt.legend()
+plt.show()
+
+#Evaluate the performance
+mse=mean_squared_error(y_test,y_pred)
+r2=r2_score(y_test,y_pred)
+print("MSE:",mse)
+print("R2:",r2)
