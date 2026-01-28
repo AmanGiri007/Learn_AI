101789f020b5d527983779a8c3ce0e6da44d8755
AmanGiri007 <giriaman4372gmail.com>
2026-01-28 19:53:46 +0545

    day4

---
+++ day4.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def sigmoid(z):
+    return 1/(1*np.exp(-z))
+
+z=np.linspace(-10,10,100)
+sigmoid_values=sigmoid(z)
+
+plt.plot(z,sigmoid_values)
+plt.title("Sigmoid Function")
+plt.xlabel("z")
+plt.ylabel("Sigma(z)")
+plt.grid()
+plt.show()
