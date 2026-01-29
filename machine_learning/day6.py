0ac9d9ce3ddda823a38e1b022586a4b9f39f61ee
AmanGiri007 <giriaman437@gmail.com>
2026-01-29 17:18:50 +0545

    #logisticsandknn

--- machine_learning/day6.py
+++ machine_learning/day6.py
@@ -4,7 +4,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score,classification_report
-
+from sklearn.linear_model import LogisticRegression
 data=load_iris()
 X,y=data.data,data.target
 
@@ -16,9 +16,24 @@ X_train=scaler.fit_transform(X_train)
 X_test=scaler.transform(X_test)
 
 #experiment with different values of k
-for k in range(1,11):
-    knn=KNeighborsClassifier(n_neighbors=k)
-    knn.fit(X_train,y_train)
-    y_pred=knn.predict(X_test)
-    accuracy=accuracy_score(y_test,y_pred)
-    print(f"k={k},Accuracy={accuracy:.2f}")
+# for k in range(1,11):
+#     knn=KNeighborsClassifier(n_neighbors=k)
+#     knn.fit(X_train,y_train)
+#     y_pred=knn.predict(X_test)
+#     accuracy=accuracy_score(y_test,y_pred)
+#     print(f"k={k},Accuracy={accuracy:.2f}")
+
+model=LogisticRegression(max_iter=200)
+model.fit(X_train,y_train)
+y_pred_log=model.predict(X_test)
+accuracy_log=accuracy_score(y_test,y_pred_log)
+print("Logistic regression accuracy:",accuracy_log)
+print("Classfication Report:",classification_report(y_test,y_pred_log))
+
+best_k=5
+knn=KNeighborsClassifier(n_neighbors=best_k)
+knn.fit(X_train,y_train)
+knn_y_pred= knn.predict(X_test)
+accuracy_knn= accuracy_score(y_test,knn_y_pred)
+print(f"KNN Accuracy: {best_k}",accuracy_knn)
+print("Classification Report:",classification_report(y_test,knn_y_pred))
